d77c1ab522d1151dd8f37b8d9268c9c1a2e2ed96
Diogo1080 <51164185+Diogo1080@users.noreply.github.com>
2020-06-20 19:26:10 +0100

    Update evento.php

--- evento.php
+++ evento.php
@@ -25,50 +25,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<link href="https://unpkg.com/tabulator-tables@4.6.3/dist/css/tabulator.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/tabulator/4.6.3/css/bootstrap/tabulator_bootstrap4.min.css" rel="stylesheet">
+	<script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.6.3/dist/js/tabulator.min.js"></script>
 </head>
 <body>
 	<div>
 		<?php require 'nav.php'; ?>
 	</div>
 	<div>
-		<form method="POST" >
-			<div>
-				<h3>
-					<?php 
-						if (isset($_GET['id'])&&isset($_GET['tipo'])) {
-							if ($_GET['tipo']=="Treino") {
-								echo "Treino: ";
-							}else{
-								echo "Jogo: ";
-							}
-						}
-					?>
-					<input hidden id="id" name="id" value="<?php 
-						if (isset($_GET['id'])&&isset($_GET['tipo'])) {
-								echo($_GET['id']);
-						}
-					?>">
-					<input hidden id="tipo" name="tipo" value="<?php 
-						if (isset($_GET['id'])&&isset($_GET['tipo'])) {
-							if ($_GET['tipo']=="Treino") {
-								echo "Treino";
-							}else{
-								echo "Jogo";
-							}
-						}
-					?>">
-					<input id="titulo" name="titulo" value="<?php 
-						if (isset($_GET['id'])&&isset($_GET['tipo'])) {
-							echo($linha['titulo']);
-						}
-					?>">
-				</h3>
-			</div>
-			<div>
-				
-			</div>
-		</form>
-	</div>
+		<div id="example-table"></div>
+<script type="text/javascript">
+	var tabledata = [
+		<?php 
+			//Busca o tempo do jogo
+			$jogo=$con->prepare("SELECT escaloes.* FROM equipas INNER JOIN escaloes ON equipas.id_escalao=escaloes.id_escalao WHERE equipas.id_equipa=$linha[id_equipa]");
+			$jogo->execute();
+			$jogo=$jogo->get_result();
+			$linha_jogo=$jogo->fetch_assoc();
+
+			//Busca os atletas do jogo
+			$atletas=$con->prepare("SELECT * FROM equipa_convocados WHERE id_jogo=$linha[id_jogo] AND id_equipa=$linha[id_equipa]");
+			$atletas->execute();
+			$atletas=$atletas->get_result();
+			
+			$linha_atletas=$atletas->fetch_assoc();
+			echo '{id:'.$linha_atletas['id_atleta'].', name:"'.$linha_atletas['id_atleta'].'"},';
+		?>
+	];
+	var table = new Tabulator("#example-table", {
+	 	height:600,
+	 	layout:"fitColumns",
+	 	data:tabledata, //assign data to table
+	 	columns:[ //Define Table Columns
+		 	{title:"Jogadores", field:"name", width:150 ,frozen:true},
+		 	<?php 
+				$minutos_passados=0;
+				$num_intervalos=0;
+				for ($i=1; $i <= $linha_jogo['duracao_jogos']; $i++) {
+				$minutos_passados+=1;
+				echo '{title:"'.$i.'", field:"minuto'.$i.'",width:10,headerSort:false,editor:"input"},';
+				if ($minutos_passados==$linha_jogo['intercalacao']) {
+					if ($num_intervalos<$linha_jogo['num_intervalos']) {
+						$duracao_intervalo=explode("-", $linha_jogo['duracao_intervalo']);
+						$duracao_intervalo=$duracao_intervalo[$num_intervalos];
+						$num_intervalos+=1;
+						echo '{title:"Intervalo ('.$duracao_intervalo.' min)", field:"Intervalo'.$num_intervalos.'",width:10,headerSort:false,headerVertical:true,editor:"input",},';
+						$minutos_passados=0;
+					}
+				}
+			}
+			?>
+	 	],
+	 	rowClick:function(e, row){ //trigger an alert message when the row is clicked
+	 		alert("Row " + row.getData().id + " Clicked!!!!");
+	 	},
+	});
+</script>
 </body>
 </html>
