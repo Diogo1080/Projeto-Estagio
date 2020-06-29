a8c72689a03ed59aebf3b8d1ad64c12606cdbb24
Daniel Figueiredo <64895435+SoRrYBrO42@users.noreply.github.com>
2020-06-29 16:50:49 +0100

    excel

--- Excel teste/gerar_planilha_especifica.php
+++ Excel teste/gerar_planilha_especifica.php
@@ -1,12 +1,12 @@
 <?php
 	session_start();
-	include_once('ligacao.php');
+	include_once('../ligacao.php');
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Contato</title>
+		<title>Relatório Especifico</title>
 	<head>
 	<body>
 		<?php
@@ -18,32 +18,29 @@
 			$html = '';
 			$html .= '<table border="1">';
 			$html .= '<tr>';
-			$html .= '<td colspan="5">Planilha Mensagem de Contatos</tr>';
+			$html .= '<td colspan="4">Relatório Especifico</tr>';
 			$html .= '</tr>';
 			
 			
 			$html .= '<tr>';
 			$html .= '<td><b>ID</b></td>';
 			$html .= '<td><b>Nome</b></td>';
-			$html .= '<td><b>E-mail</b></td>';
-			$html .= '<td><b>Assunto</b></td>';
-			$html .= '<td><b>Data</b></td>';
+			$html .= '<td><b>Sexo</b></td>';
+			$html .= '<td><b>Data de Nascimento</b></td>';
 			$html .= '</tr>';
 			
 			foreach($_POST['msg_contato'] as $id => $msg_contato){
 				//echo "ID do item: $id <br>";
 				//Selecionar todos os itens da tabela 
-				$result_msg_contatos = "SELECT * FROM mensagens_contatos WHERE id = $id LIMIT 1";
-				$resultado_msg_contatos = mysqli_query($conn , $result_msg_contatos);
+				$result_msg_contatos = "SELECT contribuintes.id_contribuinte, contribuintes.nome, contribuintes.sexo, contribuintes.dt_nasc, contribuintes.tipo_contribuinte FROM contribuintes INNER JOIN atletas ON contribuintes.id_contribuinte = atletas.id_contribuinte WHERE contribuintes.id_contribuinte = $id LIMIT 1";
+				$resultado_msg_contatos = mysqli_query($con , $result_msg_contatos);
 				
 				while($row_msg_contatos = mysqli_fetch_assoc($resultado_msg_contatos)){
 					$html .= '<tr>';
-					$html .= '<td>'.$row_msg_contatos["id"].'</td>';
+					$html .= '<td>'.$row_msg_contatos["id_contribuinte"].'</td>';
 					$html .= '<td>'.$row_msg_contatos["nome"].'</td>';
-					$html .= '<td>'.$row_msg_contatos["email"].'</td>';
-					$html .= '<td>'.$row_msg_contatos["assunto"].'</td>';
-					$data = date('d/m/Y H:i:s',strtotime($row_msg_contatos["created"]));
-					$html .= '<td>'.$data.'</td>';
+					$html .= '<td>'.$row_msg_contatos["sexo"].'</td>';
+					$html .= '<td>'.$row_msg_contatos["dt_nasc"].'</td>';
 					$html .= '</tr>';
 					;
 				}
--- Excel teste/listar_contato.php
+++ Excel teste/listar_contato.php
@@ -1,12 +1,11 @@
 <?php
-	session_start();
-	include_once('ligacao.php');
+	include_once('../ligacao.php');
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Contato</title>
+		<title>Relatorios</title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
@@ -15,38 +14,40 @@
 		<?php
 			if(isset($_POST['nome'])){
 				$pesquisar = $_POST['nome'];
-				//Selecionar  os itens da página
-				$result_msg_contatos = "SELECT * FROM mensagens_contatos WHERE nome LIKE '%$pesquisar%' LIMIT 30";
-				$resultado_msg_contatos = mysqli_query($conn , $result_msg_contatos);				
+
+				//Selecionar os itens da página
+				$result_msg_contatos = "SELECT contribuintes.id_contribuinte, contribuintes.nome, contribuintes.sexo, contribuintes.dt_nasc, contribuintes.tipo_contribuinte FROM contribuintes INNER JOIN atletas ON contribuintes.id_contribuinte = atletas.id_contribuinte WHERE contribuintes.nome LIKE '%$pesquisar%' LIMIT 30";
+				$resultado_msg_contatos = mysqli_query($con , $result_msg_contatos);		
+
 			}else{
 				//Verificar se esta sendo passado na URL a página atual, senão é atribuido a pagina
 				$pagina=(isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
 				
 				//Selecionar todos os itens da tabela 
-				$result_msg_contato = "SELECT * FROM mensagens_contatos";
-				$resultado_msg_contato = mysqli_query($conn , $result_msg_contato);
+				$result_msg_contato = "SELECT contribuintes.id_contribuinte, contribuintes.nome, contribuintes.sexo, contribuintes.dt_nasc, contribuintes.tipo_contribuinte FROM contribuintes INNER JOIN atletas ON contribuintes.id_contribuinte = atletas.id_contribuinte WHERE contribuintes.tipo_contribuinte = 'Atleta' ";
+				$resultado_msg_contato = mysqli_query($con , $result_msg_contato);				
 				
 				//Contar o total de itens
 				$total_msg_contatos = mysqli_num_rows($resultado_msg_contato);
 				
-				//Seta a quantidade de itens por página
+				//Quantidade de itens por página
 				$quantidade_pg = 20;
 				
 				//calcular o número de páginas 
 				$num_pagina = ceil($total_msg_contatos/$quantidade_pg);
 				
-				//calcular o inicio da visualizao	
+				//calcular o inicio	
 				$inicio = ($quantidade_pg*$pagina)-$quantidade_pg;
 				
-				//Selecionar  os itens da página
-				$result_msg_contatos = "SELECT * FROM mensagens_contatos limit $inicio, $quantidade_pg";
-				$resultado_msg_contatos = mysqli_query($conn , $result_msg_contatos);
+				//Selecionar os itens da página
+				$result_msg_contatos = "SELECT contribuintes.id_contribuinte, contribuintes.nome, contribuintes.sexo, contribuintes.dt_nasc, contribuintes.tipo_contribuinte FROM contribuintes INNER JOIN atletas ON contribuintes.id_contribuinte = atletas.id_contribuinte limit $inicio, $quantidade_pg";
+				$resultado_msg_contatos = mysqli_query($con , $result_msg_contatos);
 				$total_msg_contatos = mysqli_num_rows($resultado_msg_contatos);
 			}
 		?>
 		<div class="container theme-showcase" role="main">
 			<div class="page-header">
-				<h1>Lista de Mensagem Contatos</h1>
+				<h1>Lista de Jogadores</h1>
 			</div>
 			<form class="form-horizontal" method="POST" action="">
 				<div class="form-group">
@@ -62,9 +63,9 @@
 			<form method="POST" action="gerar_planilha_especifica.php">
 				<div class="row espaco">
 					<div class="pull-right">					
-						<a href="form_contato.php"><button type='button' class='btn btn-sm btn-success'>Cadastrar</button></a>
-						<a href="gerar_planilha.php"><button type='button' class='btn btn-sm btn-danger'>Gerar Excel</button></a>
-						<input type="submit" value="Excel Especifico" class='btn btn-sm btn-warning'>
+						<!--<a href="form_contato.php"><button type='button' class='btn btn-sm btn-success'>Outro</button></a>-->
+						<a href="gerar_planilha.php"><button type='button' class='btn btn-sm btn-danger'>Relatório Geral</button></a>
+						<input type="submit" value="Relatório Especifico" class='btn btn-sm btn-warning'>
 					</div>
 				</div>
 				<div class="row">
@@ -75,25 +76,23 @@
 									<th class="text-center">PDF</th>
 									<th class="text-center">Id</th>
 									<th class="text-center">Nome</th>
-									<th class="text-center">E-mail</th>
-									<th class="text-center">Assunto</th>
-									<th class="text-center">Inserido</th>
-									<th class="text-center">Ação</th>
+									<th class="text-center">Sexo</th>
+									<th class="text-center">Data de Nascimento</th>
+									<!--<th class="text-center">Ação</th>-->
 								</tr>
 							</thead>
 							<tbody>
 								<?php while($row_msg_contatos = mysqli_fetch_assoc($resultado_msg_contatos)){?>
 									<tr>
-										<?php $id = $row_msg_contatos["id"]; ?>
+										<?php $id = $row_msg_contatos["id_contribuinte"]; ?>
 										<td class="text-center">
 											<?php echo "<input type='radio' name='msg_contato[$id]' value='1'" ?>
 										</td>
-										<td class="text-center"><?php echo $row_msg_contatos["id"]; ?></td>
+										<td class="text-center"><?php echo $row_msg_contatos["id_contribuinte"]; ?></td>
 										<td class="text-center"><?php echo $row_msg_contatos["nome"]; ?></td>
-										<td class="text-center"><?php echo $row_msg_contatos["email"]; ?></td>
-										<td class="text-center"><?php echo $row_msg_contatos["assunto"]; ?></td>
-										<td class="text-center"><?php echo date('d/m/Y H:i:s',strtotime($row_msg_contatos["created"])); ?></td>
-										<td class="text-center">								
+										<td class="text-center"><?php echo $row_msg_contatos["sexo"]; ?></td>
+										<td class="text-center"><?php echo $row_msg_contatos["dt_nasc"]; ?></td>
+										<!--<td class="text-center">								
 											<a href="#">
 												<span class="glyphicon glyphicon-eye-open text-primary" aria-hidden="true"></span>
 											</a>
@@ -103,7 +102,7 @@
 											<a href="#">
 												<span class="glyphicon glyphicon-remove text-danger" aria-hidden="true"></span>
 											</a>
-										</td>
+										</td>-->
 									</tr>
 								<?php } ?>
 							</tbody>
