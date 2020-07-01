b5c470679557857381f18c8520e715dc1d5a902d
Diogo1080 <51164185+Diogo1080@users.noreply.github.com>
2020-07-01 23:04:40 +0100

    div inutil nas listas retirado

--- listar_contribuintes.php
+++ listar_contribuintes.php
@@ -6,7 +6,7 @@
       <?php include('navbar_dashboard.php'); ?>
 
       <!-- Tables dos colaboradores -->
-      <div class="col-sm-12">
+
         <div class="card"style="margin-top: 30px">
           <div class="card-header"> 
             <div class="row">
@@ -51,7 +51,6 @@
             
           </div>
         </div>
-      </div>
     </div>
    
 
