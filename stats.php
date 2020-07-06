57ad25a2cc72099ece7975527216d61b08babd1c
martimvalente <56270192+martimvalente@users.noreply.github.com>
2020-07-06 02:23:34 +0100

    Create stats.php

---
+++ stats.php
@@ -0,0 +1,17 @@
+<!-- Ligação á base de Dados -->
+<?php require('ligacao.php'); ?>
+
+<html>
+
+<!-- Ligação aos links e config da Head -->
+<?php include('head.php'); ?>
+<body>
+
+	<div class="container">
+
+        <!-- Conexão da navbar -->
+	      <?php include('navbar_dashboard.php'); ?>
+
+	      
+</body>
+</html>
